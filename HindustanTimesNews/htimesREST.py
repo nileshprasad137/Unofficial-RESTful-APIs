2c79fe6002047ef9b49b01592517e142c839be09
nileshprasad137 <prasadnilesh96@gmail.com>
2017-10-25 22:15:05 +0530

    HindustanTimes REST added(currently only one GET)

---
+++ HindustanTimesNews/htimesREST.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify
+import json
+from flask_restplus import Resource, Api
+import csv
+import requests
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)                  
+api = Api(app, version='v0', title='Hindustan Unoffial REST-API',
+    description='Conversting RSS Feed of Hindustan Times to REST API.',
+)
+ns = api.namespace('HindustanTimesAPI', description='Methods available')
+
+def loadRSS():
+ 
+    # url of rss feed
+    url = 'http://www.hindustantimes.com/rss/topnews/rssfeed.xml'
+ 
+    # creating HTTP response object from given url
+    resp = requests.get(url)
+ 
+    # saving the xml file
+    with open('topnewsfeed.xml', 'wb') as f:
+        f.write(resp.content)
+         
+ 
+def parseXML(xmlfile):
+ 
+    # create element tree object
+    tree = ET.parse(xmlfile)
+ 
+    # get root element
+    root = tree.getroot()
+ 
+    # create empty list for news items
+    newsitems = []
+ 
+    data = root.findall('./channel/item')
+    
+    # iterate news items
+    for item in root.findall('./channel/item'):
+ 
+        # empty news dictionary
+        news = {}
+ 
+        # iterate child elements of item
+        for child in item: 
+            # special checking for namespace object content:media
+            if child.tag == '{http://search.yahoo.com/mrss/}content':
+                news["media"] = child.attrib['url']
+            else:
+                news[child.tag] = child.text
+ 
+        # append news dictionary to news items list
+        newsitems.append(news)
+     
+    return newsitems
+
+
+loadRSS()
+data = parseXML('topnewsfeed.xml')
+
+@ns.route('/')                  
+class ShowAll(Resource):   
+    @ns.doc('show_all')         
+    def get(self):    
+        '''List all Courses, Tracks, Degrees'''                 
+        return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)                
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Unofficial-RESTful-APIs
 
-- [x] Udacity https://www.udacity.com/public-api/v0/courses 
-Also refer - https://s3.amazonaws.com/content.udacity-data.com/techdocs/UdacityCourseCatalogAPIDocumentation-v0.pdf
+- [x] Udacity <br\> https://www.udacity.com/public-api/v0/courses 
+<br\>Also refer - https://s3.amazonaws.com/content.udacity-data.com/techdocs/UdacityCourseCatalogAPIDocumentation-v0.pdf
 
--[] EDX
+- [] EDX
 
--[] Hindustan Times
+- [x] Hindustan Times <br\> http://www.hindustantimes.com/rss/topnews/rssfeed.xm
